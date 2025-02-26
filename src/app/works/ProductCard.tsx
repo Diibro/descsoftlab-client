e279046a5c212a6155bc867dc9c5a405e6c6ca86
clb <buntulevycaleb@gmail.com>
2025-02-26 14:26:55 +0200

    descsoftlab portfolio is done

--- src/app/works/ProductCard.tsx
+++ src/app/works/ProductCard.tsx
@@ -10,7 +10,7 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ name, image, description }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg overflow-hidden p-4">
-      <Image width={500} height={500} src={image} alt={name} className="w-full h-40 object-cover rounded-md" />
+      <Image width={500} height={500} src={image} alt={name} className="w-full h-60 object-cover rounded-md" />
       <h3 className="text-lg font-semibold text-main-blue-800 mt-3">{name}</h3>
       <p className="text-gray-600 text-sm mt-2">{description}</p>
     </div>
